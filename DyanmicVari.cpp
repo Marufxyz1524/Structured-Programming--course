e2c2360691da0ad201f47e6c31f0b68cceae5c27
Maruf109 <marufxyz1524@gmail.com>
2026-05-29 19:22:56 +0600

    how to dynamic variable declare

---
+++ DyanmicVari.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 100;
+    int *p = new int;
+    *p = x;
+    cout << *p << endl;
+    return 0;
+}
---
+++ first.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x;
+    char c;
+    cin >> x;
+    cout << x << endl;
+
+    cin >> c;
+    cout << c << endl;
+
+    cout << (char)x << endl;
+
+    return 0;
+}
